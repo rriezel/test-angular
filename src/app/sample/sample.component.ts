c9baa082d45c331cb40db2b66c45a497e9ab4b8b
rriezel <rriezel@gmail.com>
2017-05-28 21:12:27 +0900

    Initial Commit

---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample-component',
+  templateUrl: './sample.component.html',
+  //styleUrls: ['./app.component.css']
+})
+export class SampleComponent {
+  title = 'maganda ako';
+}
